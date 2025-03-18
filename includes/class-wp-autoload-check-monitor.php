943b1edbb332fc504d0660b17e1dbc276b5107cd
gcordner <geoffcordner@gmail.com>
2025-03-18 19:56:45 +0000

    variables used in monitor #330

--- includes/class-wp-autoload-check-monitor.php
+++ includes/class-wp-autoload-check-monitor.php
@@ -1,15 +1,21 @@
 <?php
 /**
- * The file that defines the monitor class for the autoload check.
+ * WP Autoload Check Monitor
  *
- * This class monitors the total autoload size in the options table
- * and sends an email alert if it exceeds a specified threshold.
+ * This file contains the WP_Autoload_Check_Monitor class which is responsible for monitoring
+ * the autoload functionality within the WordPress environment.
  *
- * @link       https://geoffcordner.net
+ * @package    WP_Autoload_Check
+ * @subpackage WP_Autoload_Check/includes
+ * @author     Geoff Cordner
+ * @license    GPL-2.0+
+ * @link       https://example.com
+ *
  * @since      1.0.0
- *
- * @package    Wp_Autoload_Check
- * @since      1.0.0
+ */
+
+/**
+ * Monitor the autoload size and send an email alert if it exceeds a threshold.
  */
 class Wp_Autoload_Check_Monitor {
 
@@ -31,16 +37,20 @@ class Wp_Autoload_Check_Monitor {
 	 * Constructor.
 	 */
 	public function __construct() {
-		$this->threshold   = 0.3;
-		$this->admin_email = 'geoffcordner@gmail.com';
+		// Get the threshold option with a fallback default of 0.3 MB.
+		$this->threshold = get_option( 'wp_autoload_check_threshold', 0.3 );
+
+		// Get the admin email option with a fallback default of the site's admin email.
+		$this->admin_email = get_option( 'wp_autoload_check_email', get_option( 'admin_email' ) );
 	}
+
 
 	/**
 	 * Execute the autoload check and send an email if needed.
 	 */
 	public function run() {
 		global $wpdb;
-
+		/* phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery */
 		delete_transient( 'autoload_size_alert' ); // Delete the transient for testing purposes.
 
 		// Calculate the total autoload size in MB.
@@ -50,9 +60,6 @@ class Wp_Autoload_Check_Monitor {
              WHERE autoload = 'yes' OR autoload = 'on'"
 		);
 
-		// Log the calculated autoload size.
-		error_log( 'Autoload size: ' . $autoload_size . ' MB' );
-
 		// If the autoload size exceeds the threshold, send an alert.
 		if ( $autoload_size > $this->threshold ) {
 			// Check if an alert has been sent recently (limit to 1 email per hour).
@@ -60,8 +67,6 @@ class Wp_Autoload_Check_Monitor {
 				$subject = 'WordPress Autoload Size Alert';
 				$message = "The autoload size is currently {$autoload_size} MB.";
 				$headers = array( 'Content-Type: text/html; charset=UTF-8' );
-
-				error_log( 'Threshold exceeded. Sending email alert.' );
 
 				wp_mail( $this->admin_email, $subject, $message, $headers );
 				// Set a transient to avoid sending another email within an hour.
