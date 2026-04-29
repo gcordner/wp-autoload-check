e502f4541d84702e31108fa0aeea2bd3a996ba42
gcordner <geoffcordner@gmail.com>
2026-04-29 12:30:41 -0700

    bump version number to 1.1

--- plk-autoload-check.php
+++ plk-autoload-check.php
@@ -15,7 +15,7 @@
  * Plugin Name:       plk autoload check
  * Plugin URI:        https://github.com/gcordner/plk-autoload-check
  * Description:       Sets specified options not to autoload, and emails an alert in autoloaded options exceed a set threshold.
- * Version:           1.0.0.1
+ * Version:           1.1
  * Author:            Geoff Cordner
  * Author URI:        https://geoffcordner.net/
  * License:           GPL-2.0+
@@ -34,7 +34,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'PLK_AUTOLOAD_CHECK_VERSION', '1.0.0' );
+define( 'PLK_AUTOLOAD_CHECK_VERSION', '1.1' );
 
 /**
  * The code that runs during plugin activation.
